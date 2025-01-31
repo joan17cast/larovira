8b17a82a4164a5d667a65e120b8aa2c970e0c626
joan.peruchet <joan17cast@gmail.com>
2025-01-31 18:10:12 +0100

    Setup tailwind animated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 	return (
 		<>
 			<div className="bg-gray-800 w-full h-screen flex flex-col items-center justify-center">
-				<h1 className=" text-gray-200 text-7xl p-4 font-semibold">
+				<h1 className=" text-gray-200 text-7xl p-4 font-semibold animate-fade-up">
 					La Rovira under construction
 				</h1>
 			</div>
